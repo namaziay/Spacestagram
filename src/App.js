1851a2c253c629b5bdd3e94b3056ac7a90fb415f
Ayaan Namazi <ayaan_namazi@hotmail.co.uk>
2022-01-09 23:23:10 -0500

    Finished Spacestagram app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState,useEffect} from 'react'
+import { CopyToClipboard } from "react-copy-to-clipboard"
+import {
+  Button
+} from "@material-ui/core"
+
+//API KEY FROM .env file
+const APIKEY = process.env.REACT_APP_API_KEY
 
 function App() {
+  const [MarsPhotos,setMarsPhotos] = useState([]);
+  const [fetched,setFetched] = useState(false);
+  const [like,setLike] = useState([]);
+  
+  
+  const likeHandler =(id)=>{
+    setLike(prev=>{
+      return [...prev,id]
+    })
+  }
+
+  const removeHandler=(id)=>{
+    const removedLike = like.filter(curr=> curr!==id);
+    setLike(removedLike);
+  }
+
+  useEffect(async()=>{
+    const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${APIKEY}`)
+    const data = await response.json()
+    setMarsPhotos(data.photos)
+    setFetched(true)
+  },[fetched])
+
+  useEffect(()=>{
+    setLike(JSON.parse(window.localStorage.getItem('like')))
+  },[])
+  useEffect(()=>{
+    window.localStorage.setItem('like',JSON.stringify(like))
+  },[like])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        fetched ?
+        <>
+          <div className="container">
+          <h2>Spacestagram</h2>
+          <h3 className ='h3'>Brought to you by NASA's image API</h3>
+          {MarsPhotos.slice(0,10).map(photo=>{
+            return(
+              <div className='post-container'>
+
+                <img src = {photo.img_src} />
+                <h3>{`${photo.rover.name}-${photo.camera.full_name}`}</h3>
+                <h5>{photo.earth_date}</h5>
+                <div>
+                {
+                like.includes(photo.id)===false?
+                <i onClick = {()=>likeHandler(photo.id)} className="material-icons">favorite</i>:
+                <i onClick = {()=>removeHandler(photo.id)} className="material-icons red-text" aria-hidden="true" style={{ color: 'red' }}>favorite</i>
+                 }
+
+                <CopyToClipboard text={photo.img_src}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                  >
+                  Copy Image URL
+                  </Button>
+                </CopyToClipboard>
+
+                </div>
+            </div>
+            )
+        })}
+          </div>
+        </>
+        :<div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+      }
+   </>
   );
 }
 
